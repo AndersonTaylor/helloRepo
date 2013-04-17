14e2e3b3db0d6eec996e7864f06468dfde614f44
AndersonTaylor <steventaylor@uky.edu>
2013-04-16 20:40:49 -0400

    Fixed imports

--- helloRepo.py
+++ helloRepo.py
@@ -1,8 +1,12 @@
-import system
-import pandas
+import sys
+import time
+#import pandas
 
-for i in range(10): 
+print time.clock()
+
+for i in range(10**3): 
 	print "hello world",
 	for j in range(2**4): 
 		print j*i,
 	print
+	print time.clock()
